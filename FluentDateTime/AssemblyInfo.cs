6286cc2387aa99793ed01c99ea1051c7d9814f47
Simon Cropp <simon.cropp@gmail.com>
2012-08-26 00:50:08 +1000

    bump version, fix license

--- FluentDateTime/AssemblyInfo.cs
+++ FluentDateTime/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 
 [assembly: AssemblyTitle("FluentDateTime")]
 [assembly: AssemblyProduct("FluentDateTime")]
-[assembly: AssemblyVersion("1.6.1.0")]
-[assembly: AssemblyFileVersion("1.6.1.0")]
+[assembly: AssemblyVersion("1.6.2.0")]
+[assembly: AssemblyFileVersion("1.6.2.0")]
+[assembly: AssemblyCopyright("http://www.apache.org/licenses/LICENSE-2.0.html")]
 
