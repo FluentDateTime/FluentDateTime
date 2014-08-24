0f478a25a560eb0a17cd0b132dd8b11485dde03d
Simon Cropp <simon.cropp@gmail.com>
2014-08-24 22:06:06 +1000

    change version

--- FluentDateTime/AssemblyInfo.cs
+++ FluentDateTime/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 
 [assembly: AssemblyTitle("FluentDateTime")]
 [assembly: AssemblyProduct("FluentDateTime")]
-[assembly: AssemblyVersion("1.10.1.0")]
-[assembly: AssemblyFileVersion("1.10.1.0")]
+[assembly: AssemblyVersion("1.11.0")]
+[assembly: AssemblyFileVersion("1.11.0")]
 [assembly: AssemblyCopyright("http://www.apache.org/licenses/LICENSE-2.0.html")]
 
