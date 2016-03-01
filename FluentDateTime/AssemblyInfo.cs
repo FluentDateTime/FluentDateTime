04beae88fb3bb2631ca14dce995dd8b7a4bc257e
Simon Cropp <simon.cropp@gmail.com>
2016-03-02 08:20:37 +1100

    bump version

--- FluentDateTime/AssemblyInfo.cs
+++ FluentDateTime/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 
 [assembly: AssemblyTitle("FluentDateTime")]
 [assembly: AssemblyProduct("FluentDateTime")]
-[assembly: AssemblyVersion("1.12.0")]
-[assembly: AssemblyFileVersion("1.12.0")]
+[assembly: AssemblyVersion("1.13.0")]
+[assembly: AssemblyFileVersion("1.13.0")]
 [assembly: AssemblyCopyright("http://www.apache.org/licenses/LICENSE-2.0.html")]
 
