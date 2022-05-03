6a9f9660751734bb088a02c99dda03aedfa198c3
Mariusz Schimke <28633575+mariusz-schimke@users.noreply.github.com>
2022-05-03 12:01:25 +0200

    Add fluent date (#103)

---
+++ src/FluentDateTime/FluentDate.cs
@@ -0,0 +1,131 @@
+namespace FluentDate;
+
+public class FluentDate
+{
+    /// <summary>
+    /// Initializes a new instance with the specified date.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="month">
+    /// The month (1 through 12).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through the number of days in <paramref name="month" />).
+    /// </param>
+    public FluentDate(int year, int month, int day) => Date = new(year, month, day, 0, 0, 0, DateTimeKind.Unspecified);
+
+#if NET6_0_OR_GREATER
+    /// <summary>
+    /// Gets the current date.
+    /// </summary>
+    public DateOnly DateOnly => new(Date.Year, Date.Month, Date.Day);
+#endif
+
+    /// <summary>
+    /// Gets midnight of the current date, with an unspecified time zone.
+    /// </summary>
+    public DateTime Date { get; }
+
+    /// <summary>
+    /// Gets midnight of the current date in the Coordinated Universal Time (UTC) time zone.
+    /// </summary>
+    public DateTime Utc => AtUtc(0);
+
+    /// <summary>
+    /// Gets midnight of the current date in the local time zone.
+    /// </summary>
+    public DateTime Local => AtLocal(0);
+
+    /// <summary>
+    /// Returns specified time at the current date, with an unspecified time zone.
+    /// </summary>
+    /// <param name="hour">
+    /// The hours (0 through 23).
+    /// </param>
+    /// <param name="minute">
+    /// The minutes (0 through 59).
+    /// </param>
+    /// <param name="second">
+    /// The seconds (0 through 59).
+    /// </param>
+    /// <param name="millisecond">
+    /// The milliseconds (0 through 999).
+    /// </param>
+    public DateTime At(int hour, int minute = 0, int second = 0, int millisecond = 0) =>
+        At(DateTimeKind.Unspecified, hour, minute, second, millisecond);
+
+    /// <summary>
+    /// Returns specified time at the current date in the Coordinated Universal Time (UTC) time zone.
+    /// </summary>
+    public DateTime AtUtc(int hour, int minute = 0, int second = 0, int millisecond = 0) =>
+        At(DateTimeKind.Utc, hour, minute, second, millisecond);
+
+    /// <summary>
+    /// Returns specified time at the current date in the local time zone.
+    /// </summary>
+    /// <param name="hour">
+    /// The hours (0 through 23).
+    /// </param>
+    /// <param name="minute">
+    /// The minutes (0 through 59).
+    /// </param>
+    /// <param name="second">
+    /// The seconds (0 through 59).
+    /// </param>
+    /// <param name="millisecond">
+    /// The milliseconds (0 through 999).
+    /// </param>
+    public DateTime AtLocal(int hour, int minute = 0, int second = 0, int millisecond = 0) =>
+        At(DateTimeKind.Local, hour, minute, second, millisecond);
+
+    /// <summary>
+    /// Returns specified time at the current date.
+    /// </summary>
+    /// <param name="kind">
+    /// One of the enumeration values that indicates whether <paramref name="hour" />, <paramref name="minute" />, <paramref name="second" />, and <paramref name="millisecond" /> specify a local time, Coordinated Universal Time (UTC), or neither.
+    /// </param>
+    /// <param name="hour">
+    /// The hours (0 through 23).
+    /// </param>
+    /// <param name="minute">
+    /// The minutes (0 through 59).
+    /// </param>
+    /// <param name="second">
+    /// The seconds (0 through 59).
+    /// </param>
+    /// <param name="millisecond">
+    /// The milliseconds (0 through 999).
+    /// </param>
+    public DateTime At(DateTimeKind kind, int hour, int minute = 0, int second = 0, int millisecond = 0) =>
+        new(Date.Year, Date.Month, Date.Day, hour, minute, second, millisecond, kind);
+
+    /// <summary>
+    /// Returns specified time at the current date and in a time zone expressed through <paramref name="offset" />.
+    /// </summary>
+    /// <param name="offset">
+    /// The time's offset from Coordinated Universal Time (UTC).
+    /// </param>
+    /// <param name="hour">
+    /// The hours (0 through 23).
+    /// </param>
+    /// <param name="minute">
+    /// The minutes (0 through 59).
+    /// </param>
+    /// <param name="second">
+    /// The seconds (0 through 59).
+    /// </param>
+    /// <param name="millisecond">
+    /// The milliseconds (0 through 999).
+    /// </param>
+    public DateTimeOffset At(TimeSpan offset, int hour, int minute = 0, int second = 0, int millisecond = 0) =>
+        new(Date.Year, Date.Month, Date.Day, hour, minute, second, millisecond, offset);
+
+#if NET6_0_OR_GREATER
+    public static implicit operator DateOnly(FluentDate date) => date.DateOnly;
+#endif
+
+    public static implicit operator DateTime(FluentDate date) => date.Date;
+    public static implicit operator DateTimeOffset(FluentDate date) => (DateTime) date;
+}
---
+++ src/FluentDateTime/IntExtensions.cs
@@ -0,0 +1,257 @@
+namespace FluentDate;
+
+public static class IntExtensions
+{
+    /// <summary>
+    /// Returns the specified day in January of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 31).
+    /// </param>
+    public static FluentDate January(this int year, int day) => new(year, 1, day);
+
+    /// <summary>
+    /// Returns the specified day in January of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 31).
+    /// </param>
+    public static FluentDate Jan(this int year, int day) => year.January(day);
+
+    /// <summary>
+    /// Returns the specified day in February of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 28 in a common year, and 29 in leap years).
+    /// </param>
+    public static FluentDate February(this int year, int day) => new(year, 2, day);
+
+    /// <summary>
+    /// Returns the specified day in February of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 28 in a common year, and 29 in leap years).
+    /// </param>
+    public static FluentDate Feb(this int year, int day) => year.February(day);
+
+    /// <summary>
+    /// Returns the specified day in March of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 31).
+    /// </param>
+    public static FluentDate March(this int year, int day) => new(year, 3, day);
+
+    /// <summary>
+    /// Returns the specified day in March of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 31).
+    /// </param>
+    public static FluentDate Mar(this int year, int day) => year.March(day);
+
+    /// <summary>
+    /// Returns the specified day in April of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 30).
+    /// </param>
+    public static FluentDate April(this int year, int day) => new(year, 4, day);
+
+    /// <summary>
+    /// Returns the specified day in April of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 30).
+    /// </param>
+    public static FluentDate Apr(this int year, int day) => year.April(day);
+
+    /// <summary>
+    /// Returns the specified day in May of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 31).
+    /// </param>
+    public static FluentDate May(this int year, int day) => new(year, 5, day);
+
+    /// <summary>
+    /// Returns the specified day in June of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 30).
+    /// </param>
+    public static FluentDate June(this int year, int day) => new(year, 6, day);
+
+    /// <summary>
+    /// Returns the specified day in June of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 30).
+    /// </param>
+    public static FluentDate Jun(this int year, int day) => year.June(day);
+
+    /// <summary>
+    /// Returns the specified day in July of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 31).
+    /// </param>
+    public static FluentDate July(this int year, int day) => new(year, 7, day);
+
+    /// <summary>
+    /// Returns the specified day in July of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 31).
+    /// </param>
+    public static FluentDate Jul(this int year, int day) => year.July(day);
+
+    /// <summary>
+    /// Returns the specified day in August of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 31).
+    /// </param>
+    public static FluentDate August(this int year, int day) => new(year, 8, day);
+
+    /// <summary>
+    /// Returns the specified day in August of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 31).
+    /// </param>
+    public static FluentDate Aug(this int year, int day) => year.August(day);
+
+    /// <summary>
+    /// Returns the specified day in September of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 30).
+    /// </param>
+    public static FluentDate September(this int year, int day) => new(year, 9, day);
+
+    /// <summary>
+    /// Returns the specified day in September of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 30).
+    /// </param>
+    public static FluentDate Sep(this int year, int day) => year.September(day);
+
+    /// <summary>
+    /// Returns the specified day in October of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 31).
+    /// </param>
+    public static FluentDate October(this int year, int day) => new(year, 10, day);
+
+    /// <summary>
+    /// Returns the specified day in October of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 31).
+    /// </param>
+    public static FluentDate Oct(this int year, int day) => year.October(day);
+
+    /// <summary>
+    /// Returns the specified day in November of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 30).
+    /// </param>
+    public static FluentDate November(this int year, int day) => new(year, 11, day);
+
+    /// <summary>
+    /// Returns the specified day in November of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 30).
+    /// </param>
+    public static FluentDate Nov(this int year, int day) => year.November(day);
+
+    /// <summary>
+    /// Returns the specified day in December of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 31).
+    /// </param>
+    public static FluentDate December(this int year, int day) => new(year, 12, day);
+
+    /// <summary>
+    /// Returns the specified day in December of the given year.
+    /// </summary>
+    /// <param name="year">
+    /// The year (1 through 9999).
+    /// </param>
+    /// <param name="day">
+    /// The day (1 through 31).
+    /// </param>
+    public static FluentDate Dec(this int year, int day) => year.December(day);
+}
