509042fa164ef5b80f264457efb86131e3d3be97
Simon Cropp <simon.cropp@gmail.com>
2024-03-02 13:04:09 +1100

    .

--- src/FluentDateTime/FluentTimeSpan.cs
+++ src/FluentDateTime/FluentTimeSpan.cs
@@ -1,5 +1,3 @@
-using System.Runtime.InteropServices;
-
 namespace FluentDate;
 
 [StructLayout(LayoutKind.Sequential)]
--- src/FluentDateTime/GlobalUsings.cs
+++ src/FluentDateTime/GlobalUsings.cs
@@ -1,3 +1,4 @@
 ﻿// Global using directives
 
 global using System.Diagnostics.Contracts;
+global using System.Runtime.InteropServices;
